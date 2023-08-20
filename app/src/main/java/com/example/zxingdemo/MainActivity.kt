799b79da368858fcfe91473ba38106682a23c784
yaoxiawen <735649296@qq.com>
2023-08-20 14:11:16 +0800

    feat:从相册中识别二维码图片

--- app/src/main/java/com/example/zxingdemo/MainActivity.kt
+++ app/src/main/java/com/example/zxingdemo/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.zxingdemo
 
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.zxing.MultiFormatReader
+import com.google.zxing.RGBLuminanceSource
 import com.google.zxing.client.android.Intents
+import com.journeyapps.barcodescanner.MixedDecoder
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanIntentResult
 import com.journeyapps.barcodescanner.ScanOptions
@@ -27,6 +34,14 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this@MainActivity, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
         }
     }
+
+    private val openGalleryRequest =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                it.data?.data?.let { uri -> handleImage(uri) }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -55,6 +70,37 @@ class MainActivity : AppCompatActivity() {
                 CustomScannerActivity::class.java
             )
             barcodeLauncher.launch(options)
+        }
+
+        findViewById<Button>(R.id.bt3).setOnClickListener {
+            openGallery()
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        openGalleryRequest.launch(Intent.createChooser(intent, "扫描二维码"))
+    }
+
+    private fun handleImage(uri: Uri) {
+        try {
+            val image = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+
+            val intArray = IntArray(image.width * image.height)
+            image.getPixels(intArray, 0, image.width, 0, 0, image.width, image.height)
+
+            val source = RGBLuminanceSource(image.width, image.height, intArray)
+            val reader = MixedDecoder(MultiFormatReader())
+            var result = reader.decode(source)
+            if (result == null) {
+                result = reader.decode(source)
+            }
+            Toast.makeText(this@MainActivity, "Scanned: ${result?.text}", Toast.LENGTH_LONG).show()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
